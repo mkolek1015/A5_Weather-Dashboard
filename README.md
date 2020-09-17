b532228cb75d0fcbcc42b3c110c947b6b3026524
mkolek1015 <mkolek7@yahoo.com>
2020-09-17 00:29:49 -0400

    final ver

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # A5_Weather-Dashboard
+
+A weather dashboard application with search functionality to find current weather conditions and the future weather outlook for multiple cities.
+
+Functional, deployed application. GitHub repository with a unique name and a README describing the project. User can search for weather reports by city using the openweathermap API. After searching for a city, the following information is displayed: Current temperature
+
+- Current humidity
+- Windspeed
+- Uv index
+- 5 day forecast
+
+Application uses icons to represent weather conditions. Application stores previously searched for cities in localstorage and displays them to the user. Application loads last searched city forecast on page load
--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,9 @@ $(document).ready(function () {
     let test = true;
     // use personal key inorder to get forecast data
     const apiKey = "bf6e0a838d371900959c489db9d941c4";
-    let url = '"http://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid=bf6e0a838d371900959c489db9d941c4';
+    let url = 'https://api.openweathermap.org/data/2.5/';
     let requestType = ""; 
     let query ="";
-
-
 
 // use JavaScript Date to display current date in a div (#displayJsDate)
 var NowDate = new Date();
@@ -19,22 +17,43 @@ var sYear = NowDate.getFullYear();
 // eDisplayDate.innerHTML = sYear+'-'+sMonth+'-'+sDay;
 
 
-document.getElementById('todaysDate').innerHTML = moment().format('');
+document.getElementById('todaysDate').innerHTML = moment().format("MMMM Do YYYY");
 // add 1 days to current date
-document.getElementById('currDatePlus1d').innerHTML = moment().add('days', 1);
+document.getElementById('currDatePlus1d').innerHTML = moment().add(1, 'days').calendar();
 // add 2 days to current date
-document.getElementById('currDatePlus2d').innerHTML = moment().add('days', 2);
+document.getElementById('currDatePlus2d').innerHTML = moment().add(2, 'days').calendar();
 // add 3 days to current date
-document.getElementById('currDatePlus3d').innerHTML = moment().add('days', 3);
+document.getElementById('currDatePlus3d').innerHTML = moment().add(3, 'days').calendar();;
 // add 4 days to current date
-document.getElementById('currDatePlus4d').innerHTML = moment().add('days', 4);
+document.getElementById('currDatePlus4d').innerHTML = moment().add(4, 'days').calendar();
 // add 5 days to current date
-document.getElementById('currDatePlus5d').innerHTML = moment().add('days', 5);
+document.getElementById('currDatePlus5d').innerHTML = moment().add(5, 'days').calendar();
 // add 6 days to current date
-document.getElementById('currDatePlus6d').innerHTML = moment().add('days', 6);
+document.getElementById('currDatePlus6d').innerHTML = moment().add(6, 'days').calendar();
 // add 7 days to current date
-document.getElementById('currDatePlus7d').innerHTML = moment().add('days', 7);
+document.getElementById('currDatePlus7d').innerHTML = moment().add(7, 'days').calendar();
 
 
+//api call
+var urlCurrent = "https://api.openweathermap.org/data/2.5/forecast?q=" + '' + "&Appid=" + apiKey + "&units=imperial"
+$.ajax({
+    url: urlCurrent,
+    method: "GET"
+    })
+    .then(function (response) {
+           //definitely use this for UV!!!!;
+        console.log(urlCurrent)
+        console.log(response)
+         $(".cur").text("Humidity: " + response.list[i].main.humidity);
+
+        })
+
+        weather.id
+        weather.icon
+        temperature.value
+        main.humidity
+        main.temp_max
+        main.temp_min
+        wind.speed
+        
 });
-
